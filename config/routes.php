09343272bd299a638cfd710429bf14589bf8adfb
mirko-pagliai <mirko.pagliai@gmail.com>
2020-12-24 15:57:45 +0100

    added routes

---
+++ config/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of me-cms-banners.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/me-cms-banners
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+
+use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\RouteBuilder;
+
+/** @var \Cake\Routing\RouteBuilder $routes */
+$routes->setRouteClass(DashedRoute::class);
+
+$routes->scope('/', ['plugin' => 'MeCms/Banners'], function (RouteBuilder $routes) {
+    //Banner
+    if (!$routes->nameExists('banner')) {
+        $routes->connect('/banner/:id', ['controller' => 'Banners', 'action' => 'open'], ['_name' => 'banner'])
+            ->setPatterns(['id' => '\d+'])
+            ->setPass(['id']);
+    }
+
+    //Admin routes
+    $routes->prefix(ADMIN_PREFIX, function (RouteBuilder $routes) {
+        $routes->setExtensions(['json']);
+
+        $routes->fallbacks('DashedRoute');
+    });
+});
